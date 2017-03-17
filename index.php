55ac3bdd3a35a3923c4d54dc7a105d963675f7bf
Langoor2 <nielsdevries@riseup.net>
2017-03-17 22:45:27 +0100

    Added initial work by /u/Hogebrink

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>Cirkeltrek - De vochtigste!</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+		<meta name="viewport" content="width=device-width" />
+		<link rel="stylesheet" type="text/css" media="only screen and (max-width: 640px), only screen and (max-device-width: 640px)" href="mobiel.css" />
+		<meta name="description" content="Cirkeltrek, de plek waar mensen hun liefde voor Willem delen." />
+		<meta name="keywords" content="willem,alexander,cirkeltrek,reddit,liefde,koning,vochtig,meem,trek,trekken,passer,lijn,cirkel" />
+	</head>
+	<body>
+		<div id="willem">
+			<small class="liefde"><a href="http://www.reddit.com/r/cirkeltrek/comments/2m2mic/willem_is_liefde_willem_is_leven/">Willem is liefde,<br>Willem is leven.</a></small>
+			<small class="maker"><a href="https://github.com/Langoor2/cirkeltrek.nl">Deze pagina op gaannaaf</a></small>
+		</div>
+		<div class="side">
+			<h3>Links (niet rechts)</h3>
+			<ul>
+				<li><a href="http://reddit.com/r/cirkeltrek">Cirkeltrek op Lashet</a></li>
+				<li><a href="https://onedrive.live.com/view.aspx?resid=A59E4E0FB7089E63!4615&ithint=file%2Cxlsx&app=Excel&authkey=!AO7T7hbQF3XdjtA">De Dikke van Willem</a></li>
+			</ul>
+			<br>
+			
+			<a href="https://www.youtube.com/watch?v=hyB_VfrESNQ"><h3>het cirkeltreklied</h3></a>
+			<iframe width="100%" height="170" src="https://www.youtube.com/embed/hyB_VfrESNQ" frameborder="0" allowfullscreen></iframe>
+			<a href="https://www.youtube.com/watch?v=5FdEzwgOmtY"><h3>Stille willem</h3></a>
+			<iframe width="100%" height="170" src="https://www.youtube.com/embed/5FdEzwgOmtY" frameborder="0" allowfullscreen></iframe>
+			<br />
+
+			<div class="sociaal">
+				<h3>sociaal en zo</h3>
+				<a href="https://discord.gg/2J27SJs">Kom meemen op Discord!</a>
+				<a href="https://telegram.me/willemsliefde">Kom meemen op Telegram!</a>
+			</div>
+		</div>
+		
+		<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	</body>
+</html>
